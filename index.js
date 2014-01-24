a6106b542135fe036a83ef1c09b3c7411ce54717
Brandon Valosek <bvalosek@gmail.com>
2014-01-23 23:41:41 -0600

    Update index file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
 
   Entity        : require('./lib/Entity.js'),
-  EntityManager : require('./lib/EntityManager.js')
+  EntityManager : require('./lib/EntityManager.js'),
+  Pool          : require('./lib/Pool.js'),
+  QuadTree      : require('./lib/QuadTree.js'),
+  Vec2          : require('./lib/Vec2.js')
 
 };
