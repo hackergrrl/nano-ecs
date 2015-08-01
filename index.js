1c076989a1515f04cc909a52166a6663ef0ad51c
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-08-01 12:54:58 -0400

    Removes ad-hoc ObjectPool. Replaces with reuse-pool.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 module.exports = {
 
   Entity        : require('./lib/Entity.js'),
-  EntityManager : require('./lib/EntityManager.js'),
-  Loop          : require('./lib/Loop.js'),
-  Messenger     : require('./lib/Messenger.js'),
-  ObjectPool    : require('./lib/ObjectPool.js'),
-  Transform     : require('./lib/Transform.js'),
-  Vec2          : require('./lib/Vec2.js')
+  EntityManager : require('./lib/EntityManager.js')
 
 };
